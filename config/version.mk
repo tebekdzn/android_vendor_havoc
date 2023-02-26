5510aebf49aa5d38eea6c98348250a6425652a0b
tebekdzn <tebekdzn@gmail.com>
2023-02-26 17:09:16 +0700

    add miksuerom branding

--- config/version.mk
+++ config/version.mk
@@ -1,7 +1,7 @@
 # Versioning System
 
 ifndef HAVOC_BUILD_TYPE
-    HAVOC_BUILD_TYPE := Unofficial
+    HAVOC_BUILD_TYPE := Beta
 endif
 
 # Only include Havoc OTA for official builds
@@ -12,14 +12,14 @@ endif
 
 TARGET_PRODUCT_SHORT := $(subst havoc_,,$(HAVOC_BUILD_TYPE))
 
-HAVOC_VERSION = 4.19
+HAVOC_VERSION = 3.0
 HAVOC_BUILD_DATE := $(shell date -u +%d-%m-%Y)
-HAVOC_FINGERPRINT := Havoc-OS/v$(HAVOC_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date -u +%Y%m%d)/$(shell date -u +%H%M)
+HAVOC_FINGERPRINT := MiksueROM/v$(HAVOC_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date -u +%Y%m%d)/$(shell date -u +%H%M)
 
 ifeq ($(WITH_GAPPS),true)
     HAVOC_BUILD_VARIANT = GApps
-    HAVOC_BUILD_VERSION := Havoc-OS-v$(HAVOC_VERSION)-$(shell date -u +%Y%m%d)-$(HAVOC_BUILD)-$(HAVOC_BUILD_TYPE)-GApps
+    HAVOC_BUILD_VERSION := MiksueROM-v$(HAVOC_VERSION)-$(shell date -u +%Y%m%d)-$(HAVOC_BUILD)-$(HAVOC_BUILD_TYPE)-GApps
 else
     HAVOC_BUILD_VARIANT = Vanilla
-    HAVOC_BUILD_VERSION := Havoc-OS-v$(HAVOC_VERSION)-$(shell date -u +%Y%m%d)-$(HAVOC_BUILD)-$(HAVOC_BUILD_TYPE)
+    HAVOC_BUILD_VERSION := MiksueROM-v$(HAVOC_VERSION)-$(shell date -u +%Y%m%d)-$(HAVOC_BUILD)-$(HAVOC_BUILD_TYPE)
 endif
